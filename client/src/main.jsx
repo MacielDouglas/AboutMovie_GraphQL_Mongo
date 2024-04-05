b18ad9ef8c0f9791b25ec8658df91adf69a2f151
MacielDouglas <douglasmaciel.dm@gmail.com>
2024-04-05 10:37:07 -0300

    refatorado vercel.json, alterado rota ApolloClient para a rota vite.config

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -3,9 +3,8 @@ import { ApolloClient, InMemoryCache, ApolloProvider } from "@apollo/client";
 import App from "./App.jsx";
 import "./index.css";
 
-// uri: "https://movie-rho-snowy.vercel.app/api/graphql",
 const client = new ApolloClient({
-  uri: "http://localhost:5000/api/graphql",
+  uri: "/graphql",
   cache: new InMemoryCache(),
   credentials: "include",
 });
@@ -15,3 +14,21 @@ ReactDOM.createRoot(document.getElementById("root")).render(
     <App />
   </ApolloProvider>
 );
+
+// import ReactDOM from "react-dom/client";
+// import { ApolloClient, InMemoryCache, ApolloProvider } from "@apollo/client";
+// import App from "./App.jsx";
+// import "./index.css";
+
+// // uri: "https://movie-rho-snowy.vercel.app/api/graphql",
+// const client = new ApolloClient({
+//   uri: "http://localhost:5000/api/graphql",
+//   cache: new InMemoryCache(),
+//   credentials: "include",
+// });
+
+// ReactDOM.createRoot(document.getElementById("root")).render(
+//   <ApolloProvider client={client}>
+//     <App />
+//   </ApolloProvider>
+// );
